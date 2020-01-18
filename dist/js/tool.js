0b227c6e51d8642767a3fc3ad50b397d523007c6
devhackerone <50499490+devhackerone@users.noreply.github.com>
2020-01-18 20:49:29 +0200

    Update tool.js

--- dist/js/tool.js
+++ dist/js/tool.js
@@ -1,1 +1,145 @@
-!function(t){var e={};function n(r){if(e[r])return e[r].exports;var o=e[r]={i:r,l:!1,exports:{}};return t[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=t,n.c=e,n.d=function(t,e,r){n.o(t,e)||Object.defineProperty(t,e,{configurable:!1,enumerable:!0,get:r})},n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,"a",e),e},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},n.p="",n(n.s=0)}([function(t,e,n){t.exports=n(1)},function(t,e){Nova.booting(function(t,e,n){var r=[];function o(t){if(!t.path.some(function(t){return t instanceof HTMLAnchorElement||t instanceof HTMLInputElement||t instanceof HTMLButtonElement||t instanceof SVGElement})&&""===window.getSelection().toString()){var e=this.querySelector('a[dusk$="-view-button"]');e&&e.click()}}Nova.$on("resources-loaded",function(){for(;r.length;)r.pop().removeEventListener("click",o);t.nextTick(function(){var t=document.querySelectorAll('table[data-testid="resource-table"] tr[dusk$="-row"]'),e=!0,n=!1,i=void 0;try{for(var c,u=t[Symbol.iterator]();!(e=(c=u.next()).done);e=!0){var a=c.value;a.style.cursor="pointer",a.addEventListener("click",o),r.push(a)}}catch(t){n=!0,i=t}finally{try{!e&&u.return&&u.return()}finally{if(n)throw i}}})})})}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(1);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+Nova.booting(function (Vue, router, store) {
+
+    var listeners = [];
+
+    function navigateToResource(event) {
+
+        var intersectsWithIgnoredElements = event.path.some(function (path) {
+            return path instanceof HTMLAnchorElement || path instanceof HTMLInputElement || path instanceof HTMLButtonElement || path instanceof SVGElement;
+        });
+
+        /**
+         * Avoid following click when clicking on A tags or when selecting text
+         */
+        if (!intersectsWithIgnoredElements && window.getSelection().toString() === '') {
+
+            var viewElement = this.querySelector('a[dusk$="-view-button"]');
+
+            if (viewElement) {
+
+                viewElement.click();
+            }
+        }
+    }
+
+    Nova.$on('resources-loaded', function () {
+
+        while (listeners.length) {
+
+            listeners.pop().removeEventListener('click', navigateToResource);
+        }
+
+        Vue.nextTick(function () {
+
+            var rows = document.querySelectorAll('table[data-testid="resource-table"] tr[dusk$="-row"]');
+
+            var _iteratorNormalCompletion = true;
+            var _didIteratorError = false;
+            var _iteratorError = undefined;
+
+            try {
+                for (var _iterator = rows[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                    var row = _step.value;
+
+
+                    row.style.cursor = 'pointer';
+                    row.addEventListener('click', navigateToResource);
+
+                    listeners.push(row);
+                }
+            } catch (err) {
+                _didIteratorError = true;
+                _iteratorError = err;
+            } finally {
+                try {
+                    if (!_iteratorNormalCompletion && _iterator.return) {
+                        _iterator.return();
+                    }
+                } finally {
+                    if (_didIteratorError) {
+                        throw _iteratorError;
+                    }
+                }
+            }
+        });
+    });
+});
+
+/***/ })
+/******/ ]);
